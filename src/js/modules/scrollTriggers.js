d5956ef04d66b1a27d1a1e1cf3d3b33a9597995d
Dmitry Conquer <dmitry.conquer@gmail.com>
2023-12-31 10:03:50 +0200

    js

--- src/js/modules/scrollTriggers.js
+++ src/js/modules/scrollTriggers.js
@@ -1,6 +1,7 @@
 const toTopButton = document.getElementById("to-top-btn");
 const mainHeader = document.getElementById("main-header");
 const hoverArea = document.getElementById("header-hover-area");
+const headerWrapper = document.querySelector(".header-wrapper");
 
 const offset = 900;
 const headerOffset = 140;
@@ -28,5 +29,26 @@ if (mainHeader && toTopButton) {
   }
   scroll();
 
+  
+  function enableHeader() {
+        const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop > headerOffset) {
+      console.log('mouseover');
+      mainHeader.classList.add("drop-header");
+    }
+  }
+  function disableHeader() {
+        const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop > headerOffset) {
+      console.log('mouseout');
+      mainHeader.classList.remove("drop-header");
+    }
+  }
+
+
+  if (headerWrapper) {
+    headerWrapper.addEventListener('mouseover', enableHeader);
+    headerWrapper.addEventListener('mouseout', disableHeader);
+  }
   window.addEventListener("scroll", scroll);
 }
